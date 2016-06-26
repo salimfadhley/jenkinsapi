2436d4073e581509adc2e495965d1d08f55cb356
Travis Carr <tmcarr89@gmail.com>
2016-06-25 17:30:29 -0700

    Test fixed

--- jenkinsapi_tests/systests/test_queue.py
+++ jenkinsapi_tests/systests/test_queue.py
@@ -90,7 +90,7 @@ class TestQueue(BaseSystemTest):
 
         queue = self.jenkins.get_queue()
         for _, item in queue.iteritems():
-            assert item.why() is StringType
+            self.assertIsInstance(item.why, str)
 
         # Clean up after ourselves
         for _, item in queue.iteritems():
