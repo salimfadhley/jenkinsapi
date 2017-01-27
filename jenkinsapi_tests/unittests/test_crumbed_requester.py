bd36061c82d5162490a1c782e6da42158b531dfe
Michael Pereira <pereira.m@gmail.com>
2017-01-27 18:49:57 -0500

    fix previous test and add new one for post_url

--- jenkinsapi_tests/unittests/test_crumbed_requester.py
+++ jenkinsapi_tests/unittests/test_crumbed_requester.py
@@ -2,18 +2,39 @@ from __future__ import print_function
 import requests
 from jenkinsapi.utils.crumb_requester import CrumbRequester
 
+
 def test_crumb_get_url(monkeypatch):
     def fake_get(*args, **kwargs):  # pylint: disable=unused-argument
         return 'SUCCESS'
 
-    def fake_crumb():
+    def fake_crumb(self): # pylint: disable=unused-argument
         return {'crumbName': 'crumbValue'}
 
     monkeypatch.setattr(requests, 'get', fake_get)
     monkeypatch.setattr(CrumbRequester, '_get_crumb_data', fake_crumb)
-    req = CrumbRequester('foo', 'bar')
+    req = CrumbRequester('foo', 'bar', baseurl='http://dummy')
 
     response = req.get_url(
+        'http://dummy',
+        params={'param': 'value'},
+        headers=None)
+
+    assert response == 'SUCCESS'
+    assert req._last_crumb_data is not None
+
+
+def test_get_url_post(monkeypatch):
+    def fake_post(*args, **kwargs):  # pylint: disable=unused-argument
+        return 'SUCCESS'
+
+    def fake_crumb(self): # pylint: disable=unused-argument
+        return {'crumbName': 'crumbValue'}
+
+    monkeypatch.setattr(requests, 'post', fake_post)
+    monkeypatch.setattr(CrumbRequester, '_get_crumb_data', fake_crumb)
+    req = CrumbRequester('foo', 'bar', baseurl='http://dummy')
+
+    response = req.post_url(
         'http://dummy',
         params={'param': 'value'},
         headers=None)
