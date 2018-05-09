90a4a9359c59237079178650a623b79c23dba512
manoj-thakkar <manoj.thakkar@nutanix.com>
2018-05-08 23:16:52 -0700

    Retrieve git repo url for a given build
    *Given a build get_repo_url shall return the git repo url of that build
    * Implemented for git only as of now

--- jenkinsapi_tests/unittests/test_build_scm_git.py
+++ jenkinsapi_tests/unittests/test_build_scm_git.py
@@ -49,3 +49,10 @@ def test_git_revision_branch(build):
     assert build.get_revision_branch()[0]['SHA1'] == \
         '7def9ed6e92580f37d00e4980c36c4d36e68f702'
     assert build.get_revision_branch()[0]['name'] == 'origin/unstable'
+
+def test_git_repo_url(self):
+    """
+    Can we extract git repo url for a given build
+    """
+    self.assertIsInstance(self.b.get_repo_url(), basestring)
+    self.assertEquals(self.b.get_repo_url(), 'https://github.com/salimfadhley/jenkinsapi.git')
