a678be9cadeb844fed80776b8e11aa188ea4d015
Hussein Kadiri <hkadiri@twitter.com>
2018-06-04 13:49:25 -0700

    removing additional blank linke

--- jenkinsapi/utils/requester.py
+++ jenkinsapi/utils/requester.py
@@ -49,7 +49,6 @@ class Requester(object):
             username = None
             password = None
 
-
         baseurl = kwargs.get('baseurl', None)
         self.base_scheme = urlparse.urlsplit(
             baseurl).scheme if baseurl else None
