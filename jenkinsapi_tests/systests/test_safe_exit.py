e4a1a68b9c3787522cf600cce7ffe097cfb28d7d
Jesper Moensted <jesper.moensted@3shape.com>
2019-04-10 16:21:49 +0200

    pytest-ordering added, one test removed

--- jenkinsapi_tests/systests/test_safe_exit.py
+++ jenkinsapi_tests/systests/test_safe_exit.py
@@ -1,20 +1,17 @@
 """
 System tests for safeExit jenkins
 NB! This test is probelmatic, as it will terminate Jenkins,
-and subsequently prevent further tests from running.
-It should probably be placed last.
+and subsequently might prevent further tests from running.
+Attempted marked as last
 
 """
 import logging
+import pytest
 
 log = logging.getLogger(__name__)
 
 
+@pytest.mark.last
 def test_safe_exit_wait(jenkins):
     jenkins.poll()  # jenkins should be alive
     jenkins.safe_exit(wait_for_exit=True)  # restart and wait for running jobs (default)
-
-
-def test_safe_exit_dont_wait(jenkins):
-    jenkins.poll()  # jenkins should be alive
-    jenkins.safe_exit(wait_for_exit=False)  # just terminate jenkins
