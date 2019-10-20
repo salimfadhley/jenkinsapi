bf8d929a98599f816b2b45c545b119ea5f5c1013
Aleksey Maksimov <ctpeko3a@gmail.com>
2019-10-20 11:05:20 +0800

    increase jenkins timeout for all tests

--- jenkinsapi_tests/systests/conftest.py
+++ jenkinsapi_tests/systests/conftest.py
@@ -124,7 +124,7 @@ def launched_jenkins():
 def jenkins(launched_jenkins):
     url = launched_jenkins.jenkins_url
 
-    jenkins_instance = Jenkins(url)
+    jenkins_instance = Jenkins(url, timeout=30)
 
     _delete_all_jobs(jenkins_instance)
     _delete_all_views(jenkins_instance)
