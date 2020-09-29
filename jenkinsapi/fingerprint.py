901d7d35435cba94d3636421aa90072fa4fd3828
Rufus Cable <rufus@threebytesfull.com>
2020-09-29 13:13:54 +0100

    Prevent MD5 errors going to the root logger.
    Use the same logger as the other log messages in this file. Also remove
    the call to basicConfig() - it should be called automatically if
    necessary.

--- jenkinsapi/fingerprint.py
+++ jenkinsapi/fingerprint.py
@@ -21,7 +21,6 @@ class Fingerprint(JenkinsBase):
     RE_MD5 = re.compile("^([0-9a-z]{32})$")
 
     def __init__(self, baseurl, id_, jenkins_obj):
-        logging.basicConfig()
         self.jenkins_obj = jenkins_obj
         assert self.RE_MD5.search(id_), ("%s does not look like "
                                          "a valid id" % id_)
@@ -56,7 +55,7 @@ class Fingerprint(JenkinsBase):
             # extract the status code from it
             response_obj = err.response
             if response_obj.status_code == 404:
-                logging.warning(
+                log.warning(
                     "MD5 cannot be checked if fingerprints are not "
                     "enabled")
                 self.unknown = True
