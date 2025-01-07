ee64d36cf58c224ff30836ce76d1998b6a2144a8
CJ Steiner <clintonsteiner@gmail.com>
2025-01-07 06:40:03 -0600

    fix other usage of pkg_resources for old python and new

--- jenkinsapi/utils/jenkins_launcher.py
+++ jenkinsapi/utils/jenkins_launcher.py
@@ -9,7 +9,6 @@ import requests
 import queue
 import threading
 import subprocess
-import importlib_resources
 from urllib3 import Retry
 from urllib.parse import urlparse
 from tarfile import TarFile
@@ -136,11 +135,20 @@ class JenkinsLancher(object):
             )
 
     def update_config(self):
-        tarball = TarFile.open(
-            fileobj=importlib_resources.files(
+        try:
+            import importlib_resources # if python > 3.11
+            # see https://setuptools.pypa.io/en/latest/pkg_resources.html
+
+            fileobj = importlib_resources.files(
                 "jenkinsapi_tests.systests", "jenkins_home.tar.gz"
             )
-        )
+        except ImportError:
+            from pkg_resources import resource_stream # below python 3.11
+
+            fileobj = resource_stream(
+                "jenkinsapi_tests.systests", "jenkins_home.tar.gz"
+            )
+        tarball = TarFile.open(fileobj=fileobj)
         tarball.extractall(path=self.jenkins_home)
 
     def install_plugins(self):
